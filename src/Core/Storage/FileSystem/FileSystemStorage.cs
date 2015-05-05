b5bfb3377f74eb9e7aecdac304f5c4650fae1604
Christopher Martin <chris.martin@eventday.com>
2015-05-05 10:54:51 -0700

    fix bad write code

--- src/Core/Storage/FileSystem/FileSystemStorage.cs
+++ src/Core/Storage/FileSystem/FileSystemStorage.cs
@@ -11,12 +11,21 @@ namespace Core.Storage.FileSystem
     {
         private readonly DirectoryInfo _directory;
         private readonly ILoggingAdapter _log;
+        private readonly JsonSerializerSettings _serializerSettings;
 
         public FileSystemStorage()
         {
             _directory = new DirectoryInfo(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "entities"));
             _directory.Create();
             _log = Context.GetLogger();
+
+            _serializerSettings = new JsonSerializerSettings
+            {
+                DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate,
+                NullValueHandling = NullValueHandling.Ignore,
+                TypeNameHandling = TypeNameHandling.None,
+                Formatting = Formatting.Indented
+            };
         }
 
         public void Handle(GetEntity message)
@@ -34,8 +43,7 @@ namespace Core.Storage.FileSystem
             using (var stream = new FileStream(info.FullName, FileMode.Open, FileAccess.Read, FileShare.None))
             using (var reader = new StreamReader(stream))
             {
-                var json = reader.ReadToEnd();
-                entity = JsonConvert.DeserializeObject(json, entityType);
+                entity = JsonConvert.DeserializeObject(reader.ReadToEnd(), entityType, _serializerSettings);
             }
 
             Sender.Tell(new EntityRetrieved(message.Key, entity));
@@ -46,12 +54,8 @@ namespace Core.Storage.FileSystem
             var fileName = GetFileName(message.Key, message.Entity.GetType());
             var info = new FileInfo(fileName);
 
-            using (var stream = new FileStream(info.FullName, FileMode.Open, FileAccess.Write, FileShare.None))
-            using (var writer = new StreamWriter(stream))
-            {
-                var json = JsonConvert.SerializeObject(message.Entity, Formatting.Indented);
-                writer.Write(json);
-            }
+            using (var file = new FileStream(info.FullName, FileMode.Open, FileAccess.Write, FileShare.None))
+                WriteToFile(file, message.Entity);
 
             _log.Info("Update entity: {0}/{1:n}", message.Entity.GetType().Name, message.Key);
             Sender.Tell(new EntityUpdated(message.Key, message.Entity));
@@ -69,11 +73,9 @@ namespace Core.Storage.FileSystem
 
             Directory.CreateDirectory(info.Directory.FullName);
 
-            using (var stream = new FileStream(info.FullName, FileMode.CreateNew, FileAccess.Write, FileShare.None))
-            using (var writer = new StreamWriter(stream))
-            {
-                writer.Write(JsonConvert.SerializeObject(message.Entity, Formatting.Indented));
-            }
+            using (var file = new FileStream(info.FullName, FileMode.CreateNew, FileAccess.Write, FileShare.None))
+                WriteToFile(file, message.Entity);
+
             Sender.Tell(new NewEntityCreated(message.Key, message.Entity));
         }
 
@@ -81,6 +83,26 @@ namespace Core.Storage.FileSystem
         {
             var folder = entityType.Name.ToLowerInvariant();
             return Path.Combine(_directory.FullName, folder, string.Format("{0:n}.json", key));
+        }
+
+        private void WriteToFile(FileStream file, object entity)
+        {
+            using (var destination = new MemoryStream())
+            {
+                Serialize(entity, destination);
+                byte[] data = destination.ToArray();
+                file.Seek(0, SeekOrigin.Begin);
+                file.Write(data, 0, data.Length);
+                // truncate this file
+                file.SetLength(data.Length);
+            }
+        }
+
+        private void Serialize(object instance, Stream destination)
+        {
+            var sw = new StreamWriter(destination);
+            sw.Write(JsonConvert.SerializeObject(instance, _serializerSettings));
+            sw.Flush();
         }
     }
 }
